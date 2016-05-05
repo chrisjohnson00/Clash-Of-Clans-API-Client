2edb0e020a60fff12994ae2b33e53c77855fc0ec
cjohnson <cjohnson@ebates.com>
2016-05-04 21:31:10 -0500

    basic env setup

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Clash-Of-Clans-API-Client
+
+## Vagrant environment setup
+
+Install vagrant from vagrantup.com
+
+Create a file called `github-oauth-token.txt` with your git hub oauth token, found on your settings page -> personal access tokens
+
+Once you've got that all setup, it's super easy to create the dev environment... just run
+
+    composer up
+    
+This will build the required environment for development.
+To connect to this VM run
+
+    composer ssh
+    
+This connects to your new vagrant vm, where you can do stuff!! WOOOHOOOO!
+
+Once connected... run
+
+    cd /vagrant
+    composer install
+    
+And this will setup all the dependencies required!!!
---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+##apt-get update
+
+### install software
+apt-get install -yf php5-dev php5-json php5-cli php5-curl
+
+## install composer
+if [ ! -f /usr/local/bin/composer ]; then
+  curl -sS https://getcomposer.org/installer | php
+  mv composer.phar /usr/local/bin/composer
+fi
+if [ ! -d "/home/vagrant/.composer" ]; then
+  mkdir /home/vagrant/.composer
+  chown -R vagrant:vagrant /home/vagrant/.composer
+  oauthToken=$(</vagrant/github-oauth-token.txt)
+  composer config -g github-oauth.github.com $oauthToken
+fi
+
+
+## install phpunit 4.8
+if [ ! -f /usr/local/bin/phpunit ]; then
+  wget --quiet https://phar.phpunit.de/phpunit-old.phar
+  mv phpunit-old.phar phpunit.phar
+  chmod +x phpunit.phar
+  mv phpunit.phar /usr/local/bin/phpunit
+fi
